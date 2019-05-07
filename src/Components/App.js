4a510b466927df33411b5e801df4ef99ab04ee9a
Alvaro <alvarodltp@gmail.com>
2019-05-06 22:39:15 -0400

    adds table to display items selected

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,56 @@
 import React from 'react';
 import '../App.css';
 import SearchBar from './SearchBar'
+import EnhancedTableHead from './EnhancedTableHead'
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      search: "",
+      resultsArr: null,
+      itemNames: null,
+      itemClicked: null,
+      arrOfItems: []
+    }
+  }
+
+  getSearchOptions = (e) => {
+    let search = e.target.value.toLowerCase()
+    fetch(`https://api.nutritionix.com/v1_1/search/${e.target.value.toLowerCase()}?results=0%3A20&cal_min=50&cal_max=100&fields=item_name%2Cbrand_name%2Cnf_serving_size_unit%2Cnf_total_carbohydrate%2Cnf_sugars%2Cimages_front_full_url%2Cnf_serving_size_qty%2Cnf_protein%2Cnf_calories%2Cnf_total_fat%2Citem_id%2Cbrand_id&appId=a8f79d5d&appKey=ad063534d80beb7b73e61da6a526265b`)
+    .then(response => response.json())
+    .then(json => {
+      let results = json["hits"].map(result => result.fields)
+      let itemNames = results.map(item => item.item_name)
+      this.setState({
+        resultsArr: results,
+        search: search,
+        itemNames: itemNames
+      })
+    })
+  }
+
+  getItem = (result) => {
+    this.setState({
+      itemClicked: result,
+      resultsArr: null
+    }, this.addItemToArr(result))
+  }
+
+  addItemToArr = (result) => {
+    this.setState({
+      arrOfItems: [...this.state.arrOfItems, result]
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <SearchBar getSearchOptions={this.getSearchOptions} getItem={this.getItem} search={this.state.search} resultsArr={this.state.resultsArr} itemNames={this.state.itemNames}/>
+        {this.state.arrOfItems.length > 0 ? <EnhancedTableHead arrOfItems={this.state.arrOfItems}/> : null }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -70,40 +70,6 @@ const styles = theme => ({
 });
 
 class SearchBar extends React.Component {
-  state = {
-    search: "",
-    resultsArr: null,
-    itemNames: null,
-    itemClicked: null
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      search: e.target.value
-    })
-  }
-
-  getSearchOptions = (e) => {
-    let search = e.target.value.toLowerCase()
-    fetch(`https://api.nutritionix.com/v1_1/search/${e.target.value.toLowerCase()}?results=0%3A20&cal_min=50&cal_max=100&fields=item_name%2Cbrand_name%2Cnf_serving_size_unit%2Cnf_total_carbohydrate%2Cnf_sugars%2Cimages_front_full_url%2Cnf_serving_size_qty%2Cnf_protein%2Cnf_calories%2Cnf_total_fat%2Citem_id%2Cbrand_id&appId=a8f79d5d&appKey=ad063534d80beb7b73e61da6a526265b`)
-    .then(response => response.json())
-    .then(json => {
-      let results = json["hits"].map(result => result.fields)
-      let itemNames = results.map(item => item.item_name)
-      this.setState({
-        resultsArr: results,
-        search: search,
-        itemNames: itemNames
-      })
-    })
-  }
-
-  getItem = (result) => {
-    this.setState({
-      itemClicked: result,
-      resultsArr: null
-    })
-  }
 
   render() {
     const { classes } = this.props;
@@ -126,7 +92,7 @@ class SearchBar extends React.Component {
                     root: classes.inputRoot,
                     input: classes.inputInput,
                   }}
-                  onChange={this.getSearchOptions}
+                  onChange={this.props.getSearchOptions}
                 />
               </div>
             </Toolbar>
@@ -135,9 +101,9 @@ class SearchBar extends React.Component {
 
         <div>
           <List className={classes.root}>
-          {this.state.resultsArr ? this.state.resultsArr.map(result =>
+          {this.props.resultsArr && this.props.search !== "" ? this.props.resultsArr.map(result =>
             <ListItem>
-              <ListItemText onClick={() => this.getItem(result)} primary={`${result.item_name} - ${result.brand_name}`} secondary={`${result.nf_serving_size_qty} ${result.nf_serving_size_unit} - ${result.nf_calories} Calories`} />
+              <ListItemText onClick={() => this.props.getItem(result)} primary={`${result.item_name} - ${result.brand_name}`} secondary={`${result.nf_serving_size_qty} ${result.nf_serving_size_unit} - ${result.nf_calories} Cal | ${result.nf_total_carbohydrate} Carb | ${result.nf_total_fat} Fat | ${result.nf_protein} Protein`}/>
             </ListItem> ) : null }
           </List>
         </div>
